72904711ac3d7dc8d0cfbc1b45b2413906923d14
RobnettSean22 <robnettsean22@gmail.com>
2020-03-30 01:58:04 +0900

    messing with the chart. changed it to a line chart

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -1,36 +1,72 @@
 import React, { Component } from "react";
-
-import { Bar, Line, Pie } from "react-chartjs-2";
+import "./Chart.scss";
+import { HorizontalBar, Line, Pie } from "react-chartjs-2";
 
 class Chart extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      chartData: {}
+      chartData: {},
+      visible: false
     };
+    this.getstuff = this.getstuff.bind(this);
   }
-  getstuff = (names, rates) => {
-    const data = {
+
+  getstuff = () => {
+    this.setState({
+      visible: true,
       chartData: {
-        labels: names,
+        labels: this.props.name,
         datasets: [
           {
-            label: "Excahnge rates",
-            data: rates,
-            backgroundColor: ["rgba(3, 12, 34, 0.75)"]
+            label: "Ratest",
+            fill: true,
+            lineTension: 0.1,
+            backgroundColor: "rgba(75,192,192,0.4)",
+            borderColor: "rgba(75,192,192,1)",
+            borderCapStyle: "butt",
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: "miter",
+            pointBorderColor: "rgba(75,192,192,1)",
+            pointBackgroundColor: "#fff",
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: "rgba(75,192,192,1)",
+            pointHoverBorderColor: "rgba(220,220,220,1)",
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: this.props.exc
           }
         ]
       }
-    };
-    return data;
+    });
   };
   render() {
     console.log(this.props.name, this.props.exc);
 
     return (
       <div>
-        <Bar data={this.getstuff} options={{}} />
+        <div id='button-case'>
+          <button onClick={this.getstuff}>Rates Chart</button>
+        </div>
+        <div className={this.state.visible === true ? "see" : "blind"}>
+          <Line
+            data={this.state.chartData}
+            options={{
+              title: {
+                display: true,
+                text: `${this.props.date} ${this.props.based} Exchange Rates`,
+                fontSize: 25
+              },
+              legend: {
+                display: false
+              }
+            }}
+          />
+        </div>
       </div>
     );
   }
