2daf5782d5bee2a171745cae493de0373d3b22e2
RobnettSean22 <robnettsean22@gmail.com>
2020-03-30 15:41:56 +0900

    adjust button outline

---
+++ .netlify/build/precache-manifest.975f079c88d202e771177741477ef996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61e1e064e8b0ed4683cab57b9b4d7b6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79da6a2b05ab9a12ab8f",
+    "url": "/static/css/main.570e39d9.chunk.css"
+  },
+  {
+    "revision": "bbbc4198dde6bc99982c",
+    "url": "/static/js/2.5a5824dd.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/static/js/2.5a5824dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79da6a2b05ab9a12ab8f",
+    "url": "/static/js/main.61ae1bfe.chunk.js"
+  },
+  {
+    "revision": "975f570b08b8d9eab0cc",
+    "url": "/static/js/runtime-main.2376dd50.js"
+  }
+]);
